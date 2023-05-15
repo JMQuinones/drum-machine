36e1a1d15b1e26f1dfdd29f92ba9609d77b27a32
JMauricioQuinonesVargas <201702063@est.umss.edu>
2023-05-15 10:04:29 -0400

    added onKeyDown event

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -2,16 +2,31 @@ import React, { useState, useEffect } from "react";
 function Key(props) {
   const [active, setActive] = useState(false);
 
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyPress);
+  }, []);
+
   const handleClick = () => {
     const audio_element = document.getElementById(props.audio.keyTrigger);
     audio_element.play();
     props.setDisplay(props.audio.id);
+  };
+  const handleKeyPress = (event) => {
+    if (event.keyCode === props.audio.keyCode) {
+      setActive(!active);
+      handleClick();
+      props.setDisplay(props.audio.id);
+      setTimeout(() => {
+        setActive(!active);
+      }, 200);
+    }
   };
 
   return (
     <div
       className={`drum-pad btn btn-secondary px-3 ${active ? "active" : ""}`}
       onClick={handleClick}
+      onKeyDown={handleKeyPress}
       id={props.audio.id}
     >
       <h1>{props.audio.keyTrigger}</h1>
